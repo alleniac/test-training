ae3684839a94431256a7f3627f768da6649b4d87
allen ma <maa@fortinet.com>
2021-10-20 15:00:30 -0700

    Get ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
+const path = require('path');
 
 app.get('/api/info', (req, res) => {
   res.send({
@@ -8,6 +9,13 @@ app.get('/api/info', (req, res) => {
     position: 'Web Developer'
   });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
